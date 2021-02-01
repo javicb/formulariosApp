35fa755443c15e4b808e6378b900ff37980d3fb8
Javi <javi.cb@gmail.com>
2021-02-01 15:20:56 +0100

    template: FormsModule

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TemplateRoutingModule } from './template-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
@@ -11,7 +12,8 @@ import { SwitchesComponent } from './switches/switches.component';
   declarations: [BasicosComponent, DinamicosComponent, SwitchesComponent],
   imports: [
     CommonModule,
-    TemplateRoutingModule
+    TemplateRoutingModule,
+    FormsModule
   ]
 })
 export class TemplateModule { }
