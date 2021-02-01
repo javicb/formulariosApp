2585328b88677d1d5f066c63c2bad6005adac4ed
Javi <javi.cb@gmail.com>
2021-02-01 14:39:37 +0100

    sidemenu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { SidemeuComponent } from './sidemeu/sidemeu.component';
 
 
@@ -7,7 +8,8 @@ import { SidemeuComponent } from './sidemeu/sidemeu.component';
 @NgModule({
   declarations: [SidemeuComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     SidemeuComponent
