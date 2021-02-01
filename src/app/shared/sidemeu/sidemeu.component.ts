84365d61cafd223cb1db2fe80069ecd8518d9f44
Javi <javi.cb@gmail.com>
2021-02-01 14:12:30 +0100

    inicio app

---
+++ src/app/shared/sidemeu/sidemeu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidemeu',
+  templateUrl: './sidemeu.component.html',
+  styles: [
+  ]
+})
+export class SidemeuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
