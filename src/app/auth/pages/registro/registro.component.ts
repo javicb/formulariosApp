1aa0d56d7eb1ab03dd03b6c9988d39615ce039b3
Javi <javi.cb@gmail.com>
2021-02-02 13:48:21 +0100

    validación email

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -10,14 +10,19 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegistroComponent implements OnInit {
 
   nombreApellidoPattenr = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattenr)]]
+    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattenr)]],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]]
   });
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    // this.miFormulario.reset({
+    //   nombre: 'Nombre'
+    // });
   }
 
   campoValido(campo: string): any {
