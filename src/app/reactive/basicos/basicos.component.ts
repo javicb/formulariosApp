bc4317b6e33369813fdec3c4f1d864d3e255e5ec
Javi <javi.cb@gmail.com>
2021-02-02 09:42:33 +0100

    primeos pasos en formularios reactivos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BasicosComponent implements OnInit {
+
+  miFormulario: FormGroup = new FormGroup({
+    nombre: new FormControl('RTW 4080')
+  });
 
   constructor() { }
 
